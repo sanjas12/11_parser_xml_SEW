100b46bc35a25c439534be4d888fd494f807079d
sanja_s <stovpyga@gmai.com>
2020-06-25 19:35:08 +0300

    update

--- 00_parser_xml.py
+++ 00_parser_xml.py
@@ -32,46 +32,51 @@ for child in root:
     temp_header = {}
     for index, i in enumerate(child):
         if i[0].text not in list_section:
-##            print(i[0].text)
+            ##            print(i[0].text)
             list_section.append(i[0].text)
-        # print(index, i.tag, len(i))
+        ##        print(index, i.tag, len(i))
         if len(i) > 0:
             for index_2, _ in enumerate(i):
-                # print('\t', index_2, _.tag, len(_))
+                ##                print('\t', index_2, _.tag, len(_))
                 if len(_) > 0:
                     for index_3, __ in enumerate(_):
-                        # print('\t\t', index_3, __.tag, len(__))
+                        ##                        print('\t\t', index_3, __.tag, len(__))
                         if len(__) > 0:
                             for index_4, ___ in enumerate(__):
-                                # print('\t\t\t', index_4, ___.tag, len(___))
+                                ##                                print('\t\t\t', index_4, ___.tag, len(___))
                                 if len(___) > 0:
                                     for index_5, ____ in enumerate(___):
-                                        # print('\t\t\t\t', index_5, ____.tag, len(____))
+                                        ##                                        print('\t\t\t\t', index_5, ____.tag, len(____))
                                         if len(____) > 0:
                                             for index_6, _____ in enumerate(____):
-                                                # print('\t\t\t\t\t', index_6, _____.tag, len(_____))
+                                                ##                                                print('\t\t\t\t\t', index_6, _____.tag, len(_____))
                                                 if len(_____) > 0:
                                                     for index_7, ______ in enumerate(_____):
-                                                        # print('\t\t\t\t\t\t', index_7, ______.tag, len(______))
+                                                        ##                                                        print('\t\t\t\t\t\t', index_7, ______.tag, len(______))
                                                         if len(______) > 0:
-                                                            for index_8, _______ in enumerate(______):
-                                                                # print('\t\t\t\t\t\t\t', index_8, _______.tag,
-                                                                #       len(_______))
-                                                                if index_8 == 1 and ______.tag == 'DokuColumn' and _____.tag == 'DokuCols' and i[0].text != 'Overview':
-                                                                    # print('\t\t\t\t\t\t\tName:___', _______.text)
-                                                                    temp_header.setdefault(_______.text, [])
+                                                            for index_8, Items in enumerate(______):
+                                                                ##                                                                print(Items.tag, Items.text)
+                                                                if len(Items) > 0 and Items[0].text == '<>' and Items[
+                                                                    1].text != 'Range does not exist':
+                                                                    temp_header.setdefault(Items[1].text, [])
+                                                                    temp_header[Items[1].text].append(Items[2].text)
+                                                                    temp_header[Items[1].text].append(Items[3].text)
+                                                                    print(Items[1].text, Items[2].text, Items[3].text)
+                                                                    print('\\' * 10)
+
         header[i[0].text] = temp_header
 
-# print(temp_header)
+##print(temp_header.keys())
 
 
+##header.pop('Overview')
+header.pop('Parameter info')
+header.pop('Scope data')
 
-header.pop('Overview')
-pprint.pprint(header)
+##pprint.pprint(header)
 
 
-
-print('list_section: ', list_section)
+##print('list_section: ', list_section)
 
 # df = pd.DataFrame(header)
 
